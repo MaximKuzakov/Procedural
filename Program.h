34053790da15a6f473b3b96b430cf8cdc7b37f8b
MaximKuzakov <66713154+MaximKuzakov@users.noreply.github.com>
2020-06-15 14:13:50 +0800

    Стандартное оформление кода для ЛР6

--- Program.h
+++ Program.h
@@ -3,41 +3,40 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-struct ccomplex
-{
+
+struct ccomplex {
     double real, imag; // действительная и мнимая части
 };
 
-struct fractions
-{
+struct fractions {
     int x, y; // числитель и знаменатель
 };
+
 //добавление нового класса 
-struct polar_coordinates
-{
+struct polar_coordinates {
     double angle;
     int distance;
 };
+
 // структура, обобщающая все имеющиеся фигуры
-struct shape
-{
+struct shape {
     // значения ключей для каждой из фигур
     enum key { CCOMPLEX, FRACTIONS, POLAR};
     key k; // ключ
-
-    union // используем прямое включение
-    {
+    union // используем прямое включение {
         ccomplex c;
         fractions f;
         polar_coordinates p;
     };
 };
+
 // Простейший контейнер на основе одномерного массива
 struct container {
     enum { max_len = 100 }; // максимальная длина
     int len; // текущая длина
     shape* cont[max_len];
 };
+
 void container_Init(container& c);
 void container_Clear(container& c);
 void container_In(container& c, ifstream& ifst);
